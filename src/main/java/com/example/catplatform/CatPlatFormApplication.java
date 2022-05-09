6eb2079cd11292323c6d1845043ae1f11ef52ad5
StarBamBi <59411333+StarBamBi@users.noreply.github.com>
2022-05-09 23:06:57 +0900

    first commit

---
+++ src/main/java/com/example/catplatform/CatPlatFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.catplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPlatFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatPlatFormApplication.class, args);
+    }
+
+}
